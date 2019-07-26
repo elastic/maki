a3583b579454c6ffa9c7edf18d473aad59da6a19
Nick Peihl <nick.peihl@elastic.co>
2019-07-26 08:32:55 -0700

    Build sdf icons in spritesheets

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const path = require('path');
-const gatherFiles = require('./scripts/gather-files');
-const iconSvgPath = path.join(__dirname, './icons');
 
 module.exports = {
   layouts: {
     all: require('./layouts/all.json')
   },
-  svgArray: gatherFiles(iconSvgPath)
+  spritesheet: {
+    1: require('./dist/sprite@1.json'),
+    2: require('./dist/sprite@2.json'),
+    4: require('./dist/sprite@4.json')
+  }
 };
